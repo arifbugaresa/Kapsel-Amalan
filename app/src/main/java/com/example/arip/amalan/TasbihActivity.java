9595384f260c1c3879aaab5f7d97afd6c0fe0d57
bugaresaa <arifyuniartofajarbugaresa@gmail.com>
2018-12-16 15:14:24 +0700

    sudah ditambahkan pada  tasbih counternya

--- app/src/main/java/com/example/arip/amalan/TasbihActivity.java
+++ app/src/main/java/com/example/arip/amalan/TasbihActivity.java
@@ -1,13 +1,44 @@
 package com.example.arip.amalan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class TasbihActivity extends AppCompatActivity {
+
+    //deklarasi tombol
+    private ImageButton Counter;
+    int qty=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasbih);
+
+        //inisialisasi Imagetombol
+        Counter = (ImageButton)findViewById(R.id.ibTambahTasbih);
+
+        //fungsi Imagetombol
+        Counter.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+             //padadang
+
+                qty = qty + 1;
+                display(qty);
+            }
+
+            private void display(int number) {
+                TextView jumlah = (TextView) findViewById(R.id.tvTasbih);
+                jumlah.setText(""+number);
+            }
+        });
+
     }
 }
